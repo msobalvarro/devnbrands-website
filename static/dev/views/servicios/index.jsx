8a6fe96049596e78586755f9a45912af83e52480
msobalvarro <mssrsobalvarro1997@gmail.com>
2019-08-14 12:46:00 -0600

    avances de construccion y diseno

--- static/dev/views/servicios/index.jsx
+++ static/dev/views/servicios/index.jsx
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react'
+import { Link } from 'react-router-dom'
+import './estilos-servicios.css'
 import services from './products.json'
 
 
 class Servicios extends PureComponent {
     state = {
         services: [],
-        
+
         // Represent url static img 
         urlStaticImage: 'static/img/icons-services'
     }
@@ -17,7 +19,7 @@ class Servicios extends PureComponent {
 
     /**this method represent a item product */
     itemProduct = ({ name, iconId, color, description }, index) => {
-        const { urlStaticImage } = this.state 
+        const { urlStaticImage } = this.state
 
         return (
             <div className="item-product" key={index}>
@@ -30,11 +32,13 @@ class Servicios extends PureComponent {
                 <p className="description">
                     {description}
                 </p>
+
+                <Link to="#" className="btn warning">Get start</Link>
             </div>
         )
     }
 
-    render () {
+    render() {
         return (
             <div id="view-services">
                 <h2>Creacion de productos, aplicaciones y servicios</h2>
