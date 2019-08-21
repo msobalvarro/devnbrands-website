c694ba7d1dcd880a7f2aa184a01f533cd7fdd3ef
msobalvarro <mssrsobalvarro1997@gmail.com>
2019-08-20 19:48:20 -0600

    avances en la vista servicio por id

--- static/dev/views/servicios/servicios.jsx
+++ static/dev/views/servicios/servicios.jsx
@@ -1,19 +1,57 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+import NavBar from '../../components/navbar'
+import Footer from '../../components/footer'
+import Data from './products.json'
 
 class ServiceById extends Component {
     state = {
-        idService: '',
-        
+        dataService: {
+            name: '',
+            iconId: '',
+            color: '',
+            description: ''
+        },
+        return: false
     }
 
     componentWillMount() {
-        console.log(this.props.match)
+        const { params } = this.props.match
+        if(params.id !== undefined) {
+            
+            Data.map(
+                (dataService) => {
+                    if(dataService.iconId === params.id) {
+                        this.setState({ dataService })
+                    }
+                }
+            )
+
+        } else {
+            this.setState({ return: true })
+        }
     }
 
     render () {
-        return (
-            <h1>Servicio</h1>
-        )
+        const { dataService } = this.state
+
+        // Validate return to services
+        if(this.state.return) {
+            return <Redirect to="/servicios" />
+        } else {
+            return (
+                <React.Fragment>
+                    <NavBar />
+                    
+                    <div id="view-service-id">
+                        <h2>{dataService.name}</h2>
+                    </div>
+
+                    <Footer />
+                </React.Fragment>
+            )
+        }
+
     }
 }
 
